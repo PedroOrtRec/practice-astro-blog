0b43f25288e11e238b44a81dd881b5fe0fbc9141
Pedro O. Rechi <pedroortegarechi@gmail.com>
2025-06-02 16:37:52 +0200

    Practicing with images. Showing post with the collection system

---
+++ src/content/config.ts
@@ -0,0 +1,18 @@
+import { z, defineCollection } from 'astro:content';
+
+const blogCollection = defineCollection({
+    type: 'content',
+    schema: ({ image }) => z.object({
+        title: z.string(),
+        date: z.date(),
+        description: z.string(),
+        image: image(),
+
+        author: z.string(),
+        tags: z.array(z.string()),
+    }),
+});
+
+export const collections = {
+    'blog': blogCollection,
+}
