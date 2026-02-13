a157fa36eba16c81d3fdea8e20cea644ef66842f
Pedro O. Rechi <pedroortegarechi@gmail.com>
2026-02-13 13:37:12 +0100

    Sanitizing and markdown

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,6 +1,10 @@
 import type { APIRoute } from "astro";
 import rss from '@astrojs/rss';
 import { getCollection } from "astro:content";
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+
+const parser = new MarkdownIt();
 
 export const GET: APIRoute = async ({ params, request, site }) => {
 
@@ -11,12 +15,25 @@ export const GET: APIRoute = async ({ params, request, site }) => {
     title: 'Practice\'s Blog',
     description: 'My blog to practice Astro',
     site,
+    xmlns: {
+      media: 'http://search.yahoo.com/mrss/',
+    },
     items: blogPosts.map( ({data, body, slug}) => ({
         title: data.title,
         link: `/posts/${slug}`,
         pubDate: data.date,
         description: data.description,
-        content: body,
+        content: sanitizeHtml(parser.render(body), {
+            allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+          }),
+          
+          customData: `<media:content
+              type="image/${data.image.format === 'jpg' ? 'jpeg' : 'png'}"
+              width="${data.image.width}"
+              height="${data.image.height}"
+              medium="image"
+              url="${site + data.image.src}" />
+          `,
     })),
     customData: '<language>es-ES</language>',
   })
